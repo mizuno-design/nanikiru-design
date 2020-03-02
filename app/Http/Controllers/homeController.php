ff58252c2fd068ec180765813686e381eb4d2d52
hiromasa <toast674@gmail.com>
2020-03-02 20:35:09 +0900

    add image and change HomeController

---
+++ app/Http/Controllers/homeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+//何切るコントローラー
+class nanikiruController extends Controller
+{
+    public function index() {
+
+    }
+}
---
+++ resources/views/nanikiru.blade.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>何切る分析</title>
+    </head>
+    <body>
+        <h1>何切る分析1</h1>
+        <form method="POST" action="/result">
+            @csrf
+            <img src="./ji1.png">
+        </form>
+    </body>
+</html>
