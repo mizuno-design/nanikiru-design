e8f9e9b6b9d2e5389efb78c020c3b92437def436
hiromasa <toast674@gmail.com>
2020-03-15 14:23:06 +0900

    db設定を修正

--- config/database.php
+++ config/database.php
@@ -51,7 +51,7 @@ return [
             'database' => env('DB_DATABASE', 'heroku_84bbcd6dda5f7a7'),
             'username' => env('DB_USERNAME', 'b722adbaf84e12'),
             'password' => env('DB_PASSWORD', '8489b38a'),
-            'unix_socket' => env('DB_SOCKET', '/Applications/MAMP/tmp/mysql/mysql.sock'),
+            'unix_socket' => env('DB_SOCKET', '/halo/mysql/mysql.sock'),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
