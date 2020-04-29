0a444924b6911c7ecb9e4b1d749d844bc6b9643e
mutya <drymtn721@gmail.com>
2020-04-30 02:26:15 +0900

    <head>タグ内の統一化@all
    <meta name="viewport">の追加
    <link rel="stylesheet">のhrefはassetヘルパーに統一

--- resources/views/description.blade.php
+++ resources/views/description.blade.php
@@ -4,14 +4,12 @@
     <head>
         <title>何切る分析</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://unpkg.com/sanitize.css"/>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500;700&display=swap">
         <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
-        <link href="https://unpkg.com/sanitize.css" rel="stylesheet"/>
-        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500;700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/card.css') }}">
         <style>
-
         </style>
     </head>
     <body>
