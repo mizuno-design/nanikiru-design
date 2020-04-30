e2a2ae0c9bbee0de29492fa1fc4a5cf5e0596790
pantyori-na <kouzu.keinaka8@gmail.com>
2020-04-30 18:00:17 +0900

    buttun　入れ替え

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -1,41 +1,49 @@
-
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>何切る分析</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
-        <style>
+<head>
+    <title>何切る分析</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        </style>
-    </head>
-    <body>
-        @include('part.header')
-        <div class="container">
-            <h1>結果</h1>
-                <div class="chart">
-                    <canvas id="myChart" width="20" height="20"></canvas>
-                </div>
-                <div class="suggestion_book">
-                    <p>あなたにおすすめの戦術書は・・・</p>
-                        <div class="book">
-                            <img src='book_images/<?php echo $result_book['path']; ?>' width=100 height=100><br>
-                            <?php echo $result_book['title']; ?>
-                        </div>
-                    <p>です</p>
-                </div>
-            <button type="button" class="btn btn-primary" onclick="location.href='description'">解答・解説を見る</button>
-            <button type="button" class="btn btn-primary" onclick="location.href='nanikiru'">戻る</button>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
+    <style>
+
+    </style>
+</head>
+
+<body>
+    @include('part.header')
+    <div class="container">
+        <h1>結果</h1>
+        <div class="chart">
+            <canvas id="myChart" width="20" height="20"></canvas>
+            　</div>
+        <div class="suggestion_book">
+            <p>あなたにおすすめの戦術書は・・・</p>
+            <div class="book">
+                <img src='book_images/<?php echo $result_book['path']; ?>' width=100 height=100><br>
+                <?php echo $result_book['title']; ?>
+            </div>
+            <p>です</p>
         </div>
-        @include('part.footer')
-        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.3/dist/Chart.min.js"></script>
-        <script>
+        <button type="button" class="btn btn-primary" onclick="location.href='nanikiru'">戻る</button>
+        <button type="button" class="btn btn-primary" onclick="location.href='description'">解答・解説を見る</button>
+    </div>
+    @include('part.footer')
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.3/dist/Chart.min.js"></script>
+    <script>
         var ctx = document.getElementById('myChart').getContext('2d');
         var question_type_array = [];
         var question_type_count = "<?php echo(count($result_array)); ?>";
@@ -52,7 +60,9 @@
                 labels: ['テンパイ時の判断', 'イーシャンテン時の判断', '孤立牌の比較', '安くて遠い手の判断', '形のセオリー', '5ブロックと6ブロックの比較'],
                 datasets: [{
                     label: '牌効率力',
-                    data: [question_type_array[0], question_type_array[1], question_type_array[2], question_type_array[3], question_type_array[4], question_type_array[5]],
+                    data: [question_type_array[0], question_type_array[1], question_type_array[2],
+                        question_type_array[3], question_type_array[4], question_type_array[5]
+                    ],
                     backgroundColor: [
                         'rgba(54, 162, 235, 0.2)',
                     ],
@@ -72,6 +82,8 @@
                 }
             }
         });
-        </script>
-    </body>
+
+    </script>
+</body>
+
 </html>
